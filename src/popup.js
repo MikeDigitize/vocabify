f4d48d837d4f3fa512426f4cf684dfe838af32a8
MikeDigitize <chad_01@hotmail.co.uk>
2019-10-05 21:53:19 +0100

    word and definition saved and retrieved from local storage

--- src/popup.js
+++ src/popup.js
@@ -1,28 +1,36 @@
+const __VOCABIFY_WORD__ = '__VOCABIFY_WORD__';
+const __VOCABIFY_DEFINITION__ = '__VOCABIFY_DEFINITION__'; 
+
+function getSelectedText(placeholder) {
+  return new Promise(function(resolve, reject) {
+    chrome.runtime.sendMessage({ action: "GET_SELECTED_TEXT" }, function(response) {
+      placeholder.textContent = response.data;
+      resolve(response.data);
+    });
+  });      
+}
+
 document.addEventListener("DOMContentLoaded", function() {
 
     const word = document.getElementById("word");
     const definition = document.getElementById("definition");
 
-    function getSelectedText(placeholder) {
-      chrome.runtime.sendMessage({ action: "GET_SELECTED_TEXT" }, function(response) {
-        placeholder.textContent = response.data;
-      });
-    }
+    const wordText = localStorage.getItem(__VOCABIFY_WORD__) || '';
+    const definitionText = localStorage.getItem(__VOCABIFY_DEFINITION__) || '';
+    
+    word.textContent = wordText;
+    definition.textContent = definitionText;
 
     document.getElementById("getWord").addEventListener("click", function() {
-      getSelectedText(word);
+      getSelectedText(word).then(function(data) {
+        localStorage.setItem(__VOCABIFY_WORD__, data);
+      });
     });
 
     document.getElementById("getDefinition").addEventListener("click", function() {
-      getSelectedText(definition);
+      getSelectedText(definition).then(function(data) {
+        localStorage.setItem(__VOCABIFY_DEFINITION__, data);
+      });
     });
 
 });
-
-function saveToLocalStorage(key, value) {
-  localStorage.setItem(key, value);
-}
-
-function retrieveFromLocalStorage(key) {
-  return localStorage.getItem(key);
-}
