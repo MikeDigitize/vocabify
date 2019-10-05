773f9c4029c92fc962187f232f47bdea2774a1ea
MikeDigitize <chad_01@hotmail.co.uk>
2019-10-05 22:42:43 +0100

    word and definition now being saved and retrieved from localstorage

--- src/popup.js
+++ src/popup.js
@@ -1,36 +1,84 @@
-const __VOCABIFY_WORD__ = '__VOCABIFY_WORD__';
-const __VOCABIFY_DEFINITION__ = '__VOCABIFY_DEFINITION__'; 
+const __VOCABIFY_WORD__ = "__VOCABIFY_WORD__";
+const __VOCABIFY_DEFINITION__ = "__VOCABIFY_DEFINITION__";
+const __VOCABIFY_SAVED_ITEMS__ = "__VOCABIFY_SAVED_ITEMS__";
 
 function getSelectedText(placeholder) {
+
   return new Promise(function(resolve, reject) {
+
     chrome.runtime.sendMessage({ action: "GET_SELECTED_TEXT" }, function(response) {
       placeholder.textContent = response.data;
       resolve(response.data);
     });
-  });      
+
+  });
 }
 
 document.addEventListener("DOMContentLoaded", function() {
 
-    const word = document.getElementById("word");
-    const definition = document.getElementById("definition");
+  const word = document.getElementById("word");
+  const definition = document.getElementById("definition");
+  const wordCount = document.getElementById("wordCount");
 
-    const wordText = localStorage.getItem(__VOCABIFY_WORD__) || '';
-    const definitionText = localStorage.getItem(__VOCABIFY_DEFINITION__) || '';
-    
-    word.textContent = wordText;
-    definition.textContent = definitionText;
+  let wordText = localStorage.getItem(__VOCABIFY_WORD__) || "";
+  let definitionText = localStorage.getItem(__VOCABIFY_DEFINITION__) || "";
+  let wordCountText;
 
-    document.getElementById("getWord").addEventListener("click", function() {
-      getSelectedText(word).then(function(data) {
-        localStorage.setItem(__VOCABIFY_WORD__, data);
-      });
+  const savedItems = localStorage.getItem(__VOCABIFY_SAVED_ITEMS__);
+  if(savedItems) {
+    wordCountText = JSON.parse(savedItems).length
+  }
+
+  word.textContent = wordText;
+  definition.textContent = definitionText;
+  wordCount.textContent = wordCountText || 0;
+
+  document
+    .getElementById("getWord")
+    .addEventListener("click", function() {
+
+      getSelectedText(word)
+        .then(function(data) {
+          localStorage.setItem(__VOCABIFY_WORD__, data);
+        });
+
+  });
+
+  document
+    .getElementById("getDefinition")
+    .addEventListener("click", function() {
+
+      getSelectedText(definition)
+        .then(function(data) {
+          localStorage.setItem(__VOCABIFY_DEFINITION__, data);
+        });
+
     });
 
-    document.getElementById("getDefinition").addEventListener("click", function() {
-      getSelectedText(definition).then(function(data) {
-        localStorage.setItem(__VOCABIFY_DEFINITION__, data);
-      });
-    });
+  document
+    .getElementById("save")
+    .addEventListener("click", function() {
+      let items = localStorage.getItem(__VOCABIFY_SAVED_ITEMS__);
+      if(!items) {
+        items = [];
+      }
+      else {
+        items = JSON.parse(items);
+      }
+      let item = {
+        word: localStorage.getItem(__VOCABIFY_WORD__),
+        definition: localStorage.getItem(__VOCABIFY_DEFINITION__)
+      };
+      items.push(item)
+      localStorage.setItem(__VOCABIFY_SAVED_ITEMS__, JSON.stringify(items));
+      wordText = "";
+      definitionText = "";
+      wordCountText = items.length;
+      localStorage.setItem(__VOCABIFY_WORD__, wordText);
+      localStorage.setItem(__VOCABIFY_DEFINITION__, definitionText);
+      word.textContent = "";
+      definition.textContent = "";
+      wordCount.textContent = wordCountText;
+    })
 
 });
