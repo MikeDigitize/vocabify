da129cc4519664f2ac0208972d768f9c2db5f1a2
MikeDigitize <chad_01@hotmail.co.uk>
2019-10-20 06:54:20 +0100

    popup tests refactored to be more readable

--- src/__tests__/popup.test.js
+++ src/__tests__/popup.test.js
@@ -5,6 +5,7 @@ import {
   isEmptyString,
   isFourHundredCharactersOrLess,
   isTwoCharactersOrMore,
+  isDefaultText,
   addToItems,
   saveItem,
   toEmptyString,
@@ -23,12 +24,18 @@ import {
   __VOCABIFY_NO_SAVED_ITEMS__
 } from '../constants';
 
-describe(`Popup tests - 
-        getValueFromStoreResponse is called with any word, definition and result values saved in storage (even when no results) 
-        to populate the popup with upon opening.
-        It gets passed the object that comes back from storage, 
-        the key to access the value and a fallback to use if there were no results found`, function() {
-  it(`...getValueFromStoreResponse should fallback to the default message if an empty object is returned back from storage`, function() {
+describe(
+  `When the popup loads, it will check storage for a word or defintion to populate the popup.
+    Chrome extension popups do not persist state in HTML - which is why state needs storing 
+    and why the check needs to be done every time it's opened.
+    Responses from the store are passed to getValueFromStoreResponse to handle all possible responses -
+    Nothing has been saved.
+    An empty string is saved.
+    A string which meets the acceptance criteria is saved.`, function() {
+
+  test(`...getValueFromStoreResponse should fallback to the default if an empty object is returned back from storage`, function() {
+    
+    // word
     let response = {};
     let wordText = getValueFromStoreResponse({
       response,
@@ -38,6 +45,7 @@ describe(`Popup tests -
 
     expect(wordText).toEqual(__VOCABIFY_NO_WORD_SELECTED__);
 
+    // definition
     response = {};
     let definitionText = getValueFromStoreResponse({
       response,
@@ -47,6 +55,7 @@ describe(`Popup tests -
 
     expect(definitionText).toEqual(__VOCABIFY_NO_DEFINITION_SELECTED__);
 
+    // saved items
     response = {};
     let savedItems = getValueFromStoreResponse({
       response,
@@ -57,7 +66,9 @@ describe(`Popup tests -
     expect(savedItems).toEqual(__VOCABIFY_NO_SAVED_ITEMS__);
   });
 
-  it(`...getValueFromStoreResponse for the word should fallback to the default message if an empty string is returned back from storage`, function() {
+  test(`...getValueFromStoreResponse for the word should fallback to the default message if an empty string is returned back from storage`, function() {
+    
+    // word
     let response = {};
     response[__VOCABIFY_WORD__] = '';
     let wordText = getValueFromStoreResponse({
@@ -68,6 +79,7 @@ describe(`Popup tests -
 
     expect(wordText).toEqual(__VOCABIFY_NO_WORD_SELECTED__);
 
+    // definition
     response = {};
     response[__VOCABIFY_DEFINITION__] = '';
     let definitionText = getValueFromStoreResponse({
@@ -78,6 +90,7 @@ describe(`Popup tests -
 
     expect(definitionText).toEqual(__VOCABIFY_NO_DEFINITION_SELECTED__);
 
+    // saved items 
     response = {};
     response[__VOCABIFY_SAVED_ITEMS__] = '';
     let savedItems = getValueFromStoreResponse({
@@ -89,7 +102,9 @@ describe(`Popup tests -
     expect(savedItems).toEqual(__VOCABIFY_NO_SAVED_ITEMS__);
   });
 
-  it(`...getValueFromStoreResponse should return the saved word, defintion or results if returned back from storage`, function() {
+  test(`...getValueFromStoreResponse should return the saved word, defintion or results if returned back from storage`, function() {
+
+    // word
     let response = {};
     let word = 'Some word';
     response[__VOCABIFY_WORD__] = word;
@@ -101,6 +116,7 @@ describe(`Popup tests -
 
     expect(wordText).toEqual(word);
 
+    // definition
     response = {};
     let definition = 'Some definition';
     response[__VOCABIFY_DEFINITION__] = definition;
@@ -112,6 +128,7 @@ describe(`Popup tests -
 
     expect(definitionText).toEqual(definition);
 
+    // saved items
     response = {};
     let items = [{ word: 'Some word', definition: 'Some definition' }];
     response[__VOCABIFY_SAVED_ITEMS__] = items;
@@ -125,12 +142,12 @@ describe(`Popup tests -
   });
 });
 
-describe(`onPopupManualTextUpdate tests - handle the manual editing of the word and definition text.
-  Should only fire if the elements have had focus first.
-  Should only save the word within if it's between 2-400 characters and isn't the default text for either.
-  If it is any of the above, the text content is set to the default and an empty string is saved.
-  The empty string ensure it won't be saved if a user attempts to`, function() {
-  it('...should not update if onFocus has not first been called before onBlur is called', async function() {
+describe(
+  `The popup allows you to manually edit the word or definition.
+    onPopupManualTextUpdate handles the manual editing of the word and definition text.`, function() {
+
+  test(`...onPopupManualTextUpdate should not update if the element has not had focus, and update if it has`, async function() {
+
     let text = 'Word';
     let key = __VOCABIFY_WORD__;
     let p = document.createElement('p');
@@ -139,6 +156,7 @@ describe(`onPopupManualTextUpdate tests - handle the manual editing of the word 
       return value;
     });
 
+    // no focus has been set
     let result = await onPopupManualTextUpdate.onBlur({
       text,
       key,
@@ -146,17 +164,22 @@ describe(`onPopupManualTextUpdate tests - handle the manual editing of the word 
       fallback,
       callback
     });
+    
     expect(result).toBe(false);
 
+    // set focus
     onPopupManualTextUpdate.onFocus();
+
     result = await onPopupManualTextUpdate.onBlur({ text, key, element: p, fallback, callback });
     expect(result).toBeTruthy();
 
+    // focus has not been set after updating
     result = await onPopupManualTextUpdate.onBlur({ text, key, element: p, fallback, callback });
     expect(result).toBe(false);
   });
 
-  it('...should update if a word between 2 and 400 characters is in the text field', async function() {
+  test('...should update if a word between 2 and 400 characters is in the text field', async function() {
+
     onPopupManualTextUpdate.onFocus();
 
     let text = 'Word';
@@ -178,9 +201,11 @@ describe(`onPopupManualTextUpdate tests - handle the manual editing of the word 
     expect(callback.mock.results[0].value).toBe(text);
     expect(result).toBe(text);
     expect(p.textContent).toBe('');
+
   });
 
-  it('...should update with an empty string if a word less than 2 or more than 400 characters is in the text field', async function() {
+  test('...should update with an empty string if a word less than 2 or more than 400 characters is in the text field', async function() {
+
     onPopupManualTextUpdate.onFocus();
 
     let text = '';
@@ -217,9 +242,11 @@ describe(`onPopupManualTextUpdate tests - handle the manual editing of the word 
     expect(callback.mock.results[1].value).toBe('');
     expect(result).toBe('');
     expect(p.textContent).toBe(fallback);
+
   });
 
-  it('...should update with an empty string if a default word is in the text field', async function() {
+  test('...should update with an empty string if a default word is in the text field', async function() {
+
     onPopupManualTextUpdate.onFocus();
 
     let text = __VOCABIFY_NO_WORD_SELECTED__;
@@ -258,41 +285,42 @@ describe(`onPopupManualTextUpdate tests - handle the manual editing of the word 
     expect(callback.mock.results[1].value).toBe('');
     expect(result).toBe('');
     expect(p.textContent).toBe(fallback);
+
   });
 });
 
-describe(`Save tests - save should only occur if -
+describe(
+  `The popup lets you save the word and definition.
+    Save should only occur if -
     the item in storage is not an empty object (i.e. not used the popup at all)
     the item is not an empty string
     the item is not the default
     the item is not less than 2 or more than 400 characters.
     If successfully saved the word and definition should return to an empty string
     and the defaults displayed to the user`, function() {
-  it(`...isEmptyObject should return true when empty and false when not`, function() {
+
+  test(`...isEmptyObject should return true when empty and false when not`, function() {
     let obj = {};
     expect(isEmptyObject(obj)).toBeTruthy();
     obj.items = [];
     expect(isEmptyObject(obj)).toBeFalsy();
   });
 
-  it(`...isEmptyString should return true when empty and false when not`, function() {
+  test(`...isEmptyString should return true when empty and false when not`, function() {
     let str = '';
     expect(isEmptyString(str)).toBeTruthy();
     str = 'Not empty';
     expect(isEmptyString(str)).toBeFalsy();
   });
 
-  it('...addToItems should add an item to the array and return it', function() {
-    let word = 'Word';
-    let definition = 'A single distinct meaningful element of speech or writing';
-    let items = [];
-    let result = addToItems({ word, definition, items });
-    expect(result).toHaveLength(1);
-    expect(result[0]).toHaveProperty('word', word);
-    expect(result[0]).toHaveProperty('definition', definition);
+  test(`...isDefaultText should return true if a string passed matches one of the defaults`, function() {
+    expect(isDefaultText(__VOCABIFY_NO_WORD_SELECTED__)).toBe(true);
+    expect(isDefaultText(__VOCABIFY_NO_DEFINITION_SELECTED__)).toBe(true);
+    expect(isDefaultText(__VOCABIFY_WORD__)).toBe(false);
+    expect(isDefaultText(__VOCABIFY_DEFINITION__)).toBe(false);
   });
 
-  it('...isFourHundredCharactersOrLess should return true if string is less than 400 or false if more', function() {
+  test('...isFourHundredCharactersOrLess should return true if string is less than 400 or false if more', function() {
     let str = '';
     for (let i = 0; i < 400; i++) {
       str += 'a';
@@ -305,25 +333,39 @@ describe(`Save tests - save should only occur if -
     expect(isFourHundredCharactersOrLess(str)).toBe(false);
   });
 
-  it('...isTwoCharactersOrMore should return true if string is more than 2 or characters or false if less', function() {
+  test('...isTwoCharactersOrMore should return true if string is more than 2 or characters or false if less', function() {
     let str = 'aa';
     expect(isTwoCharactersOrMore(str)).toBe(true);
     str = '';
     expect(isTwoCharactersOrMore(str)).toBe(false);
   });
 
-  it(`...saveItem should add word and definition as an object to the array and save to storage`, async function() {
+  test('...addToItems should add an item to the array and return it', function() {
+    let word = 'Word';
+    let definition = 'A single distinct meaningful element of speech or writing';
+    let items = [];
+    let result = addToItems({ word, definition, items });
+    expect(result).toHaveLength(1);
+    expect(result[0]).toHaveProperty('word', word);
+    expect(result[0]).toHaveProperty('definition', definition);
+  });
+
+  test(`...saveItem should add word and definition as an object to the array and save to storage`, async function() {
+
     let key = __VOCABIFY_SAVED_ITEMS__;
     let items = [
       { word: 'Word', definition: 'A single distinct meaningful element of speech or writing' }
     ];
+
     let response = {};
     response[key] = items;
+
     let callback = jest.fn(function(key, value) {
       let obj = {};
       obj[key] = value;
       return obj;
     });
+    
     let result = await saveItem({ key, items, callback });
 
     expect(callback.mock.calls).toHaveLength(1);
@@ -331,9 +373,11 @@ describe(`Save tests - save should only occur if -
     expect(callback.mock.calls[0][1]).toBe(items);
     expect(callback.mock.results[0].value).toMatchObject(response);
     expect(result).toMatchObject(response);
+
   });
 
-  it('...resetPopupAfterSave should resetPopupAfterSave the word and definition, save both and set placeholder text to default', async function() {
+  test('...resetPopupAfterSave should reset the word and definition, save both as empty strings and set placeholder text to default', async function() {
+    
     let callback = jest.fn(function(key, value) {
       return value;
     });
@@ -347,9 +391,6 @@ describe(`Save tests - save should only occur if -
     let pDefinition = document.createElement('p');
     pDefinition.textContent = definition;
 
-    expect(toEmptyString(word)).toBe('');
-    expect(toEmptyString(definition)).toBe('');
-
     await resetPopupAfterSave({ word: pWord, definition: pDefinition, callback });
 
     expect(callback.mock.calls).toHaveLength(2);
@@ -357,5 +398,6 @@ describe(`Save tests - save should only occur if -
     expect(callback.mock.results[1].value).toBe('');
     expect(pWord.textContent).toBe(__VOCABIFY_NO_WORD_SELECTED__);
     expect(pDefinition.textContent).toBe(__VOCABIFY_NO_DEFINITION_SELECTED__);
+
   });
 });
