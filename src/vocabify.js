7ad9cc2f45752093daa7e201540358f0d07e6549
MikeDigitize <chad_01@hotmail.co.uk>
2019-12-27 17:50:43 +0000

    converted render of vocab list to react

--- src/vocabify.js
+++ src/vocabify.js
@@ -1,66 +1,59 @@
 import { __VOCABIFY_SAVED_ITEMS__ } from '../src/utils/constants';
 import { getVocabifyData } from '../src/utils/general-utils'; 
 import backupData from '../data';
-import React from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 
 function App() {
-  return (
-    <h1>Hello World</h1>
-  )
+
+  document.addEventListener(
+    'visibilitychange',
+    function() {
+      if (!document.hidden) {
+        location.reload(true);
+      }
+    },
+    false
+  );
+
+  const [items, setItems] = useState([]);
+  
+  useEffect(function() {
+    async function getData() {
+      let items;
+      if(typeof chrome.storage !== 'undefined') {
+        items = await getVocabifyData(__VOCABIFY_SAVED_ITEMS__);
+      }
+      items = items && Object.keys(items).length ? items[__VOCABIFY_SAVED_ITEMS__] : backupData;
+      setItems(items);
+    }
+    getData();
+  }, []);
+
+  function createList() {
+    return items.map(function(item, index) {
+      return (
+        <article className="vocabify-item" key={`${item.word}${index}`}>
+          <h2>{ item.word }</h2>
+          <p>{ item.definition }</p>
+        </article>
+      )
+    });
+  }
+
+  let list = createList();
+
+  if(list.length) {
+    return (
+      <Fragment>
+        { list }
+      </Fragment>
+    );
+  }
+  else {
+    return null;
+  }    
+  
 }
 
 ReactDOM.render(<App />, document.getElementById('vocab'));
-
-// async function renderPage() {
-
-//   let items;
-
-//   if(typeof chrome.storage !== 'undefined') {
-//     items = await getVocabifyData(__VOCABIFY_SAVED_ITEMS__);
-//   }
-   
-//   items = items && Object.keys(items).length ? items[__VOCABIFY_SAVED_ITEMS__] : backupData;
-
-//   const root = document.getElementById('vocab');
-//   const fragment = document.createDocumentFragment();
-
-//   const dom = items.map(function(item) {
-//     const article = document.createElement('article');
-//     const h2 = document.createElement('h2');
-//     const p = document.createElement('p');
-
-//     article.classList.add('vocabify-item');
-
-//     h2.textContent = item.word;
-//     p.textContent = item.definition;
-
-//     article.appendChild(h2);
-//     article.appendChild(p);
-
-//     return article;
-//   });
-
-//   if (dom.length) {
-//     dom.forEach(function(item) {
-//       fragment.appendChild(item);
-//     });
-//     root.appendChild(fragment);
-//   } else {
-//     console.log('No items sorry!');
-//   }
-
-//   document.addEventListener(
-//     'visibilitychange',
-//     function() {
-//       if (!document.hidden) {
-//         location.reload(true);
-//       }
-//     },
-//     false
-//   );
-
-//   console.log(items);
-// }
-
-// renderPage();
