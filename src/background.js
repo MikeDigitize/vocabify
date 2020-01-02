7d9eff1023c99823285755fe6192c2d56302c524
MikeDigitize <chad_01@hotmail.co.uk>
2020-01-02 21:51:28 +0000

    background and content js files commented

--- src/background.js
+++ src/background.js
@@ -1,10 +1,20 @@
 import { backgroundUtils } from './utils/background-utils';
 let { onNewSelectedText, onRequestForSelectedText } = backgroundUtils;
 
+/**
+ * Stores any highlighted text from the web page in the closure of the backgroundUtils object,
+ * regardless of whether the user requests it.
+ * This way the text is pre-stored and ready to send
+ * via the onMessage handler below
+ */
 chrome.runtime.onConnect.addListener(function(port) {
   port.onMessage.addListener(onNewSelectedText);
 });
 
+/**
+ * Waits for the user to request the highlighted text 
+ * and sends it to the popup
+ */
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
   onRequestForSelectedText(msg, sendResponse);
 });
--- src/content.js
+++ src/content.js
@@ -1,4 +1,11 @@
 import { getChromePort, listenForHighlightedText } from './utils/content-utils';
 
+/**
+ * Creates a port to connect the web page to Vocabify background script
+ */
 const port = getChromePort();
+
+/**
+ * Waits for text to be highlighted in web page and then sends it to the background script
+ */
 listenForHighlightedText(port);
